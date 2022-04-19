66694c4e3515a4697e570d19c634986ddd611b9e
g-prog <alaobukky2@gmail.com>
2022-04-19 09:27:17 -0700

    initializing form input

---
+++ usereducer/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import FormInput from "./components/FormInput";
+import './App.css'
+function App() {
+  const [username, setUsername] = useState("");
+  console.log(username)
+  return (
+    <div className="App">
+      <form>
+        <FormInput  placeholder="Username" setUsername={setUsername}/>
+        <FormInput placeholder="Email" />
+        <FormInput placeholder="Full Name"/>
+        <FormInput  placeholder="Username"/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ usereducer/src/components/FormInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './formInput.css';
+
+const FormInput = (props) => {
+  return (
+    <div className='formInput'>
+        {/* <label>Username</label> */}
+        <input placeholder={props.placeholder} onChange={e=>props.setUsername(e.target.value)}/>
+    </div>
+  )
+}
+
+export default FormInput
